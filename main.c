c33f7afcd076075632a5d5c5f15e2a641aae2eb6
Paulo André Pimenta Aragão <andrearagao1003@outlook.com>
2020-06-06 17:37:51 -0300

    Adding Observations - How to compile on Linux

--- main.c
+++ main.c
@@ -1,26 +1,33 @@
+/*
+ * To compile on Linux, use the following command:
+ * $ gcc -pthread main.c -o main
+ */
+
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #define NUMBER_OF_THREADS 10
 
-void * print_hello_world(void *tid){
-    // Esta funcao imprime o identificador do thread e sai
+void * print_hello_world(void *tid){ // Function for print thread ID and exit
     printf("Olá, mundo! Boas vindas do thread %d\n", tid);
     pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]){
-    // O programa principal cria 10 threads e sai
+    // Program creats 10 threads and exit
+
     pthread_t threads[NUMBER_OF_THREADS];
     int status, i;
     
     for(i = 0; i < NUMBER_OF_THREADS; i++){
         printf("Metodo Main. Criando thread %d\n", i);
         
+        // If not use -pthread on compilation command, this line will generate an warning
+        // at (void *)i convertion, and an reference error of pthread_create function
         status = pthread_create(&threads[i], NULL, print_hello_world, (void *)i);
         
-        if (status != 0) {
+        if (status != 0) { // Some error had happened
             printf("Oops. pthread_create retornou um erro %d\n", status);
             exit(-1);
         }
