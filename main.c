9638cbf3367ebc9d8d2ebdb962b38f3308b21556
Victor Fernandes Gardini <victorfernandesrp@hotmail.com>
2020-06-07 21:47:31 -0300

    Some warning fixes

--- main.c
+++ main.c
@@ -10,12 +10,12 @@
 #define NUMBER_OF_THREADS 10
 
 void * print_hello_world(void *tid){ // Function for print thread ID and exit
-    //int * valor = (int *) tid;
+    // int * valor = (int *) tid;
     printf("Olá, mundo! Boas vindas do thread %d\n", tid);
     pthread_exit(NULL);
 }
 
-int main(int argc, char *argv[]){
+void main(int argc, char *argv[]){
     // Program creats 10 threads and exit
 
     pthread_t threads[NUMBER_OF_THREADS];
@@ -27,6 +27,7 @@ int main(int argc, char *argv[]){
         // If not use -pthread on compilation command, this line will generate an warning
         // at (void *)i convertion, and an reference error of pthread_create function
         status = pthread_create(&threads[i], NULL, print_hello_world, (void *) i);
+        // pthread_join(threads[i], NULL);
         
         
         if (status != 0) { // Some error had happened
@@ -39,6 +40,7 @@ int main(int argc, char *argv[]){
         This loop makes sure that the program take back its execution
         only when threads are finished
     */
+
     for(i = 0; i < NUMBER_OF_THREADS; i++)
         pthread_join(threads[i], NULL);
 
